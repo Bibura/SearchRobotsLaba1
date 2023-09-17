34451bfdc8c5ed648fd15fdd9270bb1146a76895
Yaroslav Trotkyi <73636464+Bibura@users.noreply.github.com>
2023-09-17 20:55:53 +0300

    Add files via upload

---
+++ SearchLaba1(Part2)/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        public class SearchNAU
+        {
+            private string Url { get; set; }
+            private int i = 479;
+
+            public SearchNAU(string url)
+            {
+                Url = url;
+            }
+
+            public async Task<string> SearchAsync()
+            {
+
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        while (i != 485)
+                        {
+                            HttpResponseMessage responseMessage = client.GetAsync(Url + i).Result;
+                            if (responseMessage.IsSuccessStatusCode)
+                            {
+                                string html = await responseMessage.Content.ReadAsStringAsync();
+                                var doc = new HtmlDocument();
+                                doc.LoadHtml(html);
+                                var textDivs = doc.DocumentNode.SelectNodes("//div[contains(@class, 'text')]");
+
+                                if (textDivs != null)
+                                {
+                                    foreach (var div in textDivs)
+                                    {
+                                        if (div.InnerText.Contains("Володимир Артамонов"))
+                                        {
+                                            Console.WriteLine($"FOUND MATCH : {div.InnerText}");
+                                        }
+                                    }
+                                }
+
+                            }
+                            i++;
+                        }
+                        return "Ended successfully";
+                    }
+                    catch (HttpRequestException ex)
+                    {
+                        Console.WriteLine($"HTTP error: {ex.Message}");
+                        return "Error";
+                    }
+                }
+            }
+
+
+        }
+            static async Task Main(string[] args)
+        {
+            string url = "https://nau.edu.ua/ua/news/?page=";
+
+            SearchNAU searchNAU = new SearchNAU(url);
+            Console.WriteLine(searchNAU.SearchAsync().Result);
+
+        }
+    }
+}
---
+++ SearchLaba1/SearchLaba1/Program.cs
@@ -0,0 +1,89 @@
+﻿using HtmlAgilityPack;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace SearchLaba1
+{
+    internal class Program
+    {
+
+        public class SearchSite
+        {
+            public string Url { get; set; }
+            public string OutputFilePath { get; set; }
+
+            public SearchSite(string url, string outputFilePath)
+            {
+                this.Url = url;
+                this.OutputFilePath = outputFilePath;
+            }
+
+            public async Task<string> searchForNewsAsync()
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        HttpResponseMessage responseMessage = client.GetAsync(Url).Result;
+
+                        if (responseMessage.IsSuccessStatusCode)
+                        {
+                            string html = await responseMessage.Content.ReadAsStringAsync();
+                            HashSet<string> uniqueURLs = new HashSet<string>();
+
+                            HtmlDocument doc = new HtmlDocument();
+                            doc.LoadHtml(html);
+
+                            HtmlNodeCollection links = doc.DocumentNode.SelectNodes("//a[@href]");
+
+                            if (links != null)
+                            {
+                                foreach (var link in links)
+                                {
+                                    string href = link.GetAttributeValue("href", "");
+
+                                    if (Regex.IsMatch(href, "novini", RegexOptions.IgnoreCase))
+                                    {
+                                        if (!href.Contains("category/novini"))
+                                        {
+                                            uniqueURLs.Add(href);
+                                        }
+                                    }
+                                }
+                            }
+                            foreach (var item in uniqueURLs)
+                            {
+                                Console.WriteLine($"URL : {item}");
+                            }
+                            File.WriteAllLines(OutputFilePath, uniqueURLs);
+                            Console.WriteLine($"URLs was written up in file");
+                            return "true";
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error: {responseMessage.StatusCode}");
+                            return "false";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return "false";
+                    }
+                }
+
+            }
+
+        }
+
+        static async Task Main(string[] args)
+        {
+            string url = "http://ccs.nau.edu.ua/";
+            string outputFilePath = "output.txt";
+
+            SearchSite searchSite = new SearchSite(url, outputFilePath);
+
+            Console.WriteLine(searchSite.searchForNewsAsync().Result);
+        }
+    }
+}
